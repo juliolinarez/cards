3413b7931abfa05f526b9759238d40f4de32c896
Sophie-Sergio <san.storres@gmail.com>
2025-09-03 22:56:37 -0400

    chore: add active storage

--- db/migrate/20250904022526_create_active_storage_tables.active_storage.rb
+++ db/migrate/20250904022526_create_active_storage_tables.active_storage.rb
@@ -1,5 +1,5 @@
 # This migration comes from active_storage (originally 20170806125915)
-class CreateActiveStorageTables < ActiveRecord::Migration[7.0]
+class CreateActiveStorageTables < ActiveRecord::Migration[8.0]
   def change
     # Use Active Record's configured type for primary and foreign keys
     primary_key_type, foreign_key_type = primary_and_foreign_key_types
@@ -12,12 +12,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[7.0]
       t.string   :service_name, null: false
       t.bigint   :byte_size,    null: false
       t.string   :checksum
-
-      if connection.supports_datetime_with_precision?
-        t.datetime :created_at, precision: 6, null: false
-      else
-        t.datetime :created_at, null: false
-      end
+      t.datetime :created_at, precision: 6, null: false
 
       t.index [ :key ], unique: true
     end
@@ -26,12 +21,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[7.0]
       t.string     :name,     null: false
       t.references :record,   null: false, polymorphic: true, index: false, type: foreign_key_type
       t.references :blob,     null: false, type: foreign_key_type
-
-      if connection.supports_datetime_with_precision?
-        t.datetime :created_at, precision: 6, null: false
-      else
-        t.datetime :created_at, null: false
-      end
+      t.datetime :created_at, precision: 6, null: false
 
       t.index [ :record_type, :record_id, :name, :blob_id ], name: :index_active_storage_attachments_uniqueness, unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
